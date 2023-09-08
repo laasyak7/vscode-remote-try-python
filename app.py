824254edf5edf6c3a37cad71c75d843eb6c29486
Laasya Krupa <107047395+laasyak7@users.noreply.github.com>
2023-09-08 07:30:16 +0000

    game w computer

--- app.py
+++ app.py
@@ -7,5 +7,41 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return app.send_static_file("index.html")
+
+
+# write a game which plays rock paper and scissor game through console with user
+# write a game which plays rock paper and scissor game through console with user
+
+def play_game():
+    print("Welcome to Rock Paper Scissor Game")
+    print("Please enter your choice")
+    print("1. Rock")
+    print("2. Paper")
+    print("3. Scissor")
+    user_choice = int(input("Enter your choice: "))
+    print("Your choice is ", user_choice)
+    print("Computer is choosing...")
+    import random
+    computer_choice = random.randint(1, 3)
+    print("Computer choice is ", computer_choice)
+    if user_choice == computer_choice:
+        print("Game is draw")
+    elif user_choice == 1 and computer_choice == 2:
+        print("Computer wins")
+    elif user_choice == 1 and computer_choice == 3:
+        print("User wins")
+    elif user_choice == 2 and computer_choice == 1:
+        print("User wins")
+    elif user_choice == 2 and computer_choice == 3:
+        print("Computer wins")
+    elif user_choice == 3 and computer_choice == 1:
+        print("Computer wins")
+    elif user_choice == 3 and computer_choice == 2:
+        print("User wins")
+    else:
+        print("Invalid choice")
+
+play_game()
+
+
+
